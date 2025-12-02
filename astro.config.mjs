1d630e738ecff9817ef248db94d8d8d1a5a3043d
Predrag Drljaca <hello@toodear.rocks>
2025-12-02 12:55:10 +0100

    Fix: Remove Tailwind mention from across config files

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,9 @@
 import { defineConfig } from "astro/config";
-import tailwindcss from "@tailwindcss/vite";
 import react from "@astrojs/react";
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  /* integrations: [tailwind(), react(), mdx()], */
   integrations: [react(), mdx()],
-  vite: {
-    plugins: [tailwindcss()],
-  },
+  vite: {},
 });
